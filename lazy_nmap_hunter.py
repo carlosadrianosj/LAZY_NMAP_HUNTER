4144ea2caf776a80b38af9128bc5e18ab56f53d9
Carlos Adriano de Souza Jorge <71354599+carlosadrianosj@users.noreply.github.com>
2021-01-05 21:25:38 -0300

    Update lazy_nmap_hunter.py

--- lazy_nmap_hunter.py
+++ lazy_nmap_hunter.py
@@ -48,18 +48,14 @@ print('\033[35m'+'''
 
 #verifica se o programa foi executado em modo root
 permissao_do_usuario = os.geteuid()
-if permissao_do_usuario == 1000:
-      print("              Este programa precisa ser executado em modo ROOT!!")
-      time.sleep(2)
-      print("              Este programa precisa ser executado em modo ROOT!!")
-      time.sleep(2)
-      print("              Este programa precisa ser executado em modo ROOT!!")
-      time.sleep(2)
-      print("                  Exemplo: sudo python3 lazy_nmap_hunter.py\n\n\n\n")
-      time.sleep(2)
-      os._exit()
-elif permissao_do_usuario == 0:
-      pass
+if permissao_do_usuario != 0:
+    for i in range(5):
+        print("              Este programa precisa ser executado em modo ROOT!!\n\n")
+        time.sleep(0.5)        
+    print("                 Exemplo: sudo python3 lazy_nmap_hunter.py")
+    os._exit(0)      
+else:
+    pass
 
 #inicia o laço infinito na ferramenta
 comando = True
